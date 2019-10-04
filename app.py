b3e26be0aacf71bd90e1ced8044842fe8c4bb71f
PpimpechH <51944575+PpimpechH@users.noreply.github.com>
2019-10-04 12:01:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     a=os.environ['Authorization']
-    return "นางสาวพิมพ์เพชร เพชรมาก เลขที่ 1 4 ชั้น ม.4/9"
+    return "นางสาวพิมพ์เพชร เพชรมาก เลขที่ 14 ชั้น ม.4/9"
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
@@ -20,19 +20,19 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
-    userText = decoded['queryResult']['intent']['displayName']
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']#user = decoded["events"][0]['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']#userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-    if (userText == 'Hellooo') :
-        sendText(user,'จ้าาาาา')
+    if   (userText == 'Hellooo') :
+        sendText(user,'จ้าาาา')
     elif (userText == 'Ba bye') :
-        sendText(user,'Byeee')
+        sendText(user,'byeeeeeee')
     else :
-        sendText(user,'Yassss')
+        sendText(user,'Yassssss')
     return '',200
-   
+
 def sendText(user, text):
-  LINE_API = 'http://api.line.me/v2/bot/message/reply'
+  LINE_API = 'https://api.line.me/v2/bot/message/reply'
   headers = {
     'Content-Type': 'application/json; charset=UTF-8',
     'Authorization': os.environ['Authorization']    # ตั้ง Config vars ใน heroku พร้อมค่า Access token
